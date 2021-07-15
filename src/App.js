11d6a1edd7aa6ce50d696c619ff589235451d83f
Nirupan Joe <nirupanjoej@gmail.com>
2021-07-15 11:12:05 +0000

    itempane and price matrix as grid

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import context from './core/context';
 import SampleService from './services/sample';
 import ItemPane from './components/itemPane';
 import PriceMatrix from './components/priceMatrix';
+import { Box, Grid } from '@material-ui/core';
 
 const App = () => {
 	useEffect(SampleService.sayHai, []);
-	// eslint-disable-next-line no-console
-	console.log(context.state);
 
 	return (
-		<div className={ `App ${ context.state.theme }` }>
-			<div> { ItemPane() } </div>
-			<div> { PriceMatrix() } </div>
-		</div>
+		<Box className={ `App ${ context.state.theme }` }>
+			<Grid container="true">
+				<Grid item="true"xs={ 6 }> { ItemPane() } </Grid>
+				<Grid item="true"xs={ 6 }> { PriceMatrix() } </Grid>
+			</Grid>
+		</Box>
 	);
 };
 
--- src/components/itemPane.js
+++ src/components/itemPane.js
@@ -6,12 +6,12 @@ import { Box } from '@material-ui/core';
 import ItemInput from './itemInput';
 
 const BoxComponent = (component) =>
-	<Box key={ component }my={ 2 }> {component}</Box>;
+	<Box key={ component }py={ 2 }> {component}</Box>;
 
 const ItemPane = () => {
 	const components = [ItemInput(), Item(), Filter(), ThemeSelect()];
 
-	return <Box id="item-pane">
+	return <Box id="item-pane"height="100vh"overflow="auto">
 		{components.map(BoxComponent)}
 	</Box>;
 };
