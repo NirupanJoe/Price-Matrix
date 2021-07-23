0a647dc50bfdb02fbfa0ae5b00e48a18ea2847d4
nirupan <nirupanjoej@gmail.com>
2021-07-23 13:56:49 +0530

    Introduced a tooltip

--- src/components/addButton.js
+++ src/components/addButton.js
@@ -1,18 +1,20 @@
-import { Button } from '@material-ui/core';
+import { Button, Tooltip } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
 import { React } from 'react';
 import context from '../core/context';
 
 const AddButton = () =>
-	<Button
-		variant="contained"
-		color="secondary"
-		size="large"
-		disabled={ context.state.price === '' }
-		onClick={ context.actions.addItemAndPrice }
-		endIcon={ <Add/> }
-	>
-		Add
-	</Button>;
+	<Tooltip title="Add">
+		<Button
+			variant="contained"
+			color="secondary"
+			size="large"
+			disabled={ context.state.price === '' }
+			onClick={ context.actions.addItemAndPrice }
+			endIcon={ <Add/> }
+		>
+			Add
+		</Button>
+	</Tooltip>;
 
 export default AddButton;
--- src/components/editButton.js
+++ src/components/editButton.js
@@ -1,18 +1,20 @@
-import { Button } from '@material-ui/core';
+import { Button, Tooltip } from '@material-ui/core';
 import { Edit } from '@material-ui/icons';
 import { React } from 'react';
 import context from '../core/context';
 
 const EditButton = () =>
-	<Button
-		variant="outlined"
-		color="primary"
-		size="large"
-		disabled={ context.state.edit === null }
-		onClick={ context.actions.editItem }
-		startIcon={ <Edit/> }
-	>
-		Edit
-	</Button>;
+	<Tooltip title="Edit">
+		<Button
+			variant="outlined"
+			color="primary"
+			size="large"
+			disabled={ context.state.edit === null }
+			onClick={ context.actions.editItem }
+			startIcon={ <Edit/> }
+		>
+			Edit
+		</Button>
+	</Tooltip>;
 
 export default EditButton;
